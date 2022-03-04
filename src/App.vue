c5cb7e846566c4eb18e71bea58ef8a610418b620
abdullahb53 <abdullahb5355@gmail.com>
2022-03-04 18:04:20 +0300

    vue-cli-eklendi-component-eklendi

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  
+  
+  <MapPath />
+
+</template>
+
+<script>
+
+import MapPath from './components/MapPath.vue'
+
+
+export default {
+
+  components: {
+    MapPath
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
